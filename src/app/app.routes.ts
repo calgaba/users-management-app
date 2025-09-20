db98e2639f8619e5143dc421b24b271f8c57d2dd
calgaba <calgaba.practicas@gmail.com>
2025-09-20 12:18:58 +0200

    initial components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: 'newuser', component: UserFormComponent },
+  { path: 'updateuser/:id', component: UserFormComponent },
+  { path: '**', redirectTo: 'home' }
+
+
+
+
+];
